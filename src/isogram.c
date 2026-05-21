48350d1656b092a77372388e13592e4aeb2ca74e
Nika Vardidze <nika.vardidze26@gmail.com>
2026-05-21 13:08:25 +0000

    Unit Tests

--- src/isogram.c
+++ src/isogram.c
@@ -3,12 +3,29 @@
 #include "isogram.h"
 #include <string.h>
 #include <stdio.h>
+#include <inttypes.h>
 
 
 bool is_isogram(const char word[]){
-    // this is the documentation
-    if (false) {
-        printf("hello world\n");
+    if(strlen(word) == 0)
+        return true;
+    uint32_t seen[256] = {0};
+
+    for(int i = 0; word[i] != '\0';i++)
+    {
+        seen[word[i]]++;
+    }
+
+    uint32_t expected = 0;
+    for(int i = 0;i<256;i++)
+    {
+        if(seen[i] != 0)
+        {
+            if (expected == 0)
+                expected = seen[i];
+            else if(seen[i] != expected)
+                return false;
+        }
     }
     return true;
 }
--- test/test_isogram.c
+++ test/test_isogram.c
@@ -12,11 +12,21 @@ void test_empty_string(void) {
     TEST_ASSERT_TRUE(is_isogram("")); 
 }
 
+void test_upper_cased_isogram(void){
+    TEST_ASSERT_TRUE(is_isogram("OTTO"))
+}
+
+void test_mixed_cased_isogram(void){
+    TEST_ASSERT_TRUE(is_isogram("OTTo"))
+    TEST_ASSERT_TRUE(is_isogram("OTto"))
+}
+
 int main(void) {
     UnityBegin("isIsogram");
 
     RUN_TEST(test_empty_string);
-
+    RUN_TEST(test_upper_cased_isogram);
+    RUN_TEST(test_mixed_cased_isogram);
     UnityEnd();
     return 0;
 }
